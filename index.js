d08a72406f80b27d2b08e074656a264b4248e6dd
EasyDev-Papuga <148044294+EasyDev-Papuga@users.noreply.github.com>
2024-09-19 17:02:41 +0300

    1.0

---
+++ index.js
@@ -0,0 +1,28 @@
+let button = document.getElementsByClassName('main-button')[0]
+let scoreIH = document.getElementsByClassName('score')[0]
+let audio = new Audio('sound/song.mp3')
+let button_img = document.getElementsByClassName('button-img')[0]
+
+score = 0
+
+
+
+button.addEventListener('click', (event) => {
+    audio.play()
+    score+=1
+    scoreIH.innerHTML = `Score: ${score}`
+    button_img.style.scale = 0.1
+    setTimeout(()=>{
+        button_img.style.scale = 0.2
+    }, 50)
+    setTimeout(()=>{
+        button_img.style.scale = 0.3
+    }, 100)
+    setTimeout(()=>{
+        button_img.style.scale = 0.4
+    }, 150)
+    setTimeout(()=>{
+        button_img.style.scale = 0.5
+    }, 200)
+    
+})
