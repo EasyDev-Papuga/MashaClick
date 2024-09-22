cf7b8736a6908df94f7fbe46d614d2876675b83d
Papuga <148044294+EasyDev-Papuga@users.noreply.github.com>
2024-09-22 16:54:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,47 +4,51 @@ let audio = new Audio('sound/song.mp3')
 let button_img = document.getElementsByClassName('button-img')[0]
 
 let score = 0
+let isBanned = "false"
+let started = false
+let cps = 0
+let clicks = 0
 
-function delay(f) {
-
-  let isCooldown = false;
-
-  return function() {
-    if (isCooldown) return;
-
-    f.apply(this, arguments);
-
-    isCooldown = true;
-
-    setTimeout(() => isCooldown = false, 100);
-  };
-
-}
-
-const onClick = () => {
-    if(localStorage.getItem('scoresv')){
-        score = parseInt(localStorage.getItem('scoresv'))
+button.addEventListener('click', (event) => {
+    if(localStorage.getItem("ban")){
+        isBanned = localStorage.getItem("ban")
     }
-
-    audio.play()
-    score+=1
-    scoreIH.innerHTML = `Score: ${score}`
-
-    localStorage.setItem('scoresv', score)
-
-    button_img.style.scale = 0.1
     setTimeout(()=>{
-        button_img.style.scale = 0.2
-    }, 50)
-    setTimeout(()=>{
-        button_img.style.scale = 0.3
-    }, 100)
-    setTimeout(()=>{
-        button_img.style.scale = 0.4
-    }, 150)
-    setTimeout(()=>{
-        button_img.style.scale = 0.5
-    }, 200)
-}
-
-button.addEventListener('click', delay(onClick))
+        cps = clicks
+        clicks = 0
+        console.log(cps)
+        if(cps>= 10){
+            localStorage.setItem("ban", "true")
+        }
+    },1000)
+    if(isBanned == "false"){
+        if(localStorage.getItem('scoresv')){
+            score = parseInt(localStorage.getItem('scoresv'))
+        }
+    
+        audio.play()
+        score += 1
+        clicks += 1
+        scoreIH.innerHTML = `Score: ${score}`
+    
+        localStorage.setItem('scoresv', score)
+    
+        button_img.style.scale = 0.1
+        setTimeout(()=>{
+            button_img.style.scale = 0.2
+        }, 50)
+        setTimeout(()=>{
+            button_img.style.scale = 0.3
+        }, 100)
+        setTimeout(()=>{
+            button_img.style.scale = 0.4
+        }, 150)
+        setTimeout(()=>{
+            button_img.style.scale = 0.5
+        }, 200)
+    }
+    else{
+        scoreIH.innerHTML = `You have get banned`
+    }
+    
+})
